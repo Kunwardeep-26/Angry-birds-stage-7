423458de830d4276b996c3483b247a2ff4e431f5
Kunwardeep-26 <kunwar1212juneja@gmail.com>
2020-12-19 17:57:43 +0530

    added trajectory

--- Bird.js
+++ Bird.js
@@ -2,6 +2,9 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    // creating an empty array to store the positions where the bird has moved
+    this.trajectory = [];
   }
 
   display() {
@@ -9,5 +12,17 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    // storing the positions only when the velocity and the position of the bird is above a threshold
+    // else there would be too much smoke, even after the bird has stopped 
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+      // getting the x and y position of the bird for each frame
+      var position = [this.body.position.x, this.body.position.y];
+      // storing the positions as an array inside the trajectory array
+      this.trajectory.push(position);
+    }
+    
+    for(var i = 0; i<this.trajectory.length; i++){
+      image(this.smokeImage,this.trajectory[i][0], this.trajectory[i][1]);
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,63 @@
+/*
+//Examples of diiferent data types in JavaScript
+
+//String data type
+var string = "hello";
+console.log(string);
+
+//Number data type
+var num = 56;
+console.log(num);
+
+//Boolean data type
+var bool = false;
+console.log(bool);
+
+//Undefined data type
+var object;
+console.log(object);
+
+//Reassigning the same undefined object to null
+object = null;
+console.log(object);
+
+//array with values of same data type
+var arr1 = [34, 56.7, 89];
+console.log(arr1);
+
+//array with values of different data type
+var arr2 = [23, "world", true, 78.9, "bird", null];
+console.log(arr2);
+
+//length of the array
+console.log(arr2.length);
+
+//accessing the array elements through index of the element
+console.log(arr2[3]); //78.9
+console.log(arr2[0]); //23
+console.log(arr2[5]); //null
+console.log(arr2[arr2.length-1]);//null
+
+//array inside an array
+var arr3 = [45, 98, "angry", [56, 0, "kunwar"], ["pretty", "good", "girl"]];
+console.log(arr3);
+console.log(arr3.length); //5
+console.log(arr3[2]); //angry
+console.log(arr3[3][0]); //56
+console.log(arr3[4][2]); //girl
+
+//push new elements inside an array
+arr3.push(1000);
+arr3.push(34);
+arr3.push(["aishwarya"], 67, 28);
+console.log(arr3);
+
+//removing the last element of array
+arr3.pop();
+console.log(arr3);
+
+*/
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,6 +67,8 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+
+var gameState = "onSling";
 
 
 function preload() {
@@ -69,16 +131,20 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState !== "launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+    
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        // slingshot.attach(bird.body);
     }
 }
